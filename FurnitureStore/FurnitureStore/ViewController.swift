0f4d1b8599ffae97c9a975d9e743d13c5065244c
Pavel Boltromyuk <pavel.boltromyuk99@gmail.com>
2022-01-25 22:27:20 +0300

    added xcode project

---
+++ FurnitureStore/FurnitureStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FurnitureStore
+//
+//  Created by Pavel Boltromyuk on 25.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
